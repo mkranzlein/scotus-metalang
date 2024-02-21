fcefcfd9b1d508864ecb56eec1532f1d42a8c63c
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-02-21 13:22:02 -0500

    Added placeholders for missing steps

--- scripts/inference.py
+++ scripts/inference.py
@@ -2,7 +2,7 @@
 
 Load sentence-segmented CAP data and run it through model.
 """
-
+import json
 import torch
 from transformers import BertTokenizerFast
 
@@ -14,9 +14,19 @@ sentence_model = torch.load("models/curiam/sentence_level_model_nohipool.pt")
 token_model = torch.load("models/token_classification_model.pt")
 bert_tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased', do_lower_case=True)
 
-def predict_opinion():
+
+def predict_opinion(filepath):
     """Classifies all tokens in an opinion"""
-    pass
+    with open(filepath, "r") as f:
+        case = json.load(f)
+        text = case["text"]
+        paragraphs = text.split("\n")
+        # Sentence segment paragraphs
+
+        # Predict each sentence
+
+        # Save predictions or return
+
 
 def predict_sentence_toks(sentence: list[str]):
     x = bert_tokenizer(sentence, is_split_into_words=True, return_attention_mask=True,
@@ -24,8 +34,8 @@ def predict_sentence_toks(sentence: list[str]):
     output = token_model(x["input_ids"].cuda(), mask=x["attention_mask"].cuda(),
                          token_type_ids=x["token_type_ids"].cuda())
     sigmoid_outputs = torch.nn.functional.sigmoid(output)
-    
-#    for token, preds in zip(bert_tokenizer.convert_ids_to_tokens(x["input_ids"][0]), sigmoid_outputs[0]):
+    # TODO: return predictions
+    # for token, preds in zip(bert_tokenizer.convert_ids_to_tokens(x["input_ids"][0]), sigmoid_outputs[0]):
 
 
 sample_sentence = ["This", "is", "a", "sentence"]
