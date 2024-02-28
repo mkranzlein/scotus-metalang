54c7bef55ab4ad5f9b0338b0db30d21842821c55
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-02-28 13:16:24 -0500

    Refactored with db statuses

--- src/scotus_metalang/cap.py
+++ src/scotus_metalang/cap.py
@@ -5,6 +5,11 @@ import os
 import requests
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+from scotus_metalang.authors import AUTHOR_MAP
+
+load_dotenv()
 CAP_TOKEN = os.environ["CAP_TOKEN"]
 
 
@@ -38,64 +43,91 @@ def id_of_longest_casebody(api_response: dict) -> int:
     return case_id
 
 
-def save_opinions_by_docket_number(docket_number, log, log_writer):
-    """Saves all opinions associated with a case specified by docket number."""
-    # TODO: Add reprocess functionality to re-download data already logged.
-    if docket_number in log:
-        return  # Docket number already processed if in log
+def process_opinions_by_docket_number(docket_number: str) -> dict:
+    """Saves opinions if possible. Returns dict of info to update db."""
     api_response = cases_by_docket_number(docket_number)
-    count = api_response["count"]
-    if count == 0:  # Case not found
-        log_writer.writerow({"docket_number": docket_number, "status": "not_found", "cases_returned": count,
-                            "case_id_selected": None, "num_opinions": None, "authors": None})
-        return
+
+    # Case not found
+    if api_response["count"] == 0:
+        db_params = {"case_status": "not_found",
+                     "is_authors_known": 0,
+                     "selected_case_id": None,
+                     "num_opinions": None,
+                     "authors": None,
+                     "docket_number": docket_number}
+        return db_params
 
     case_id = id_of_longest_casebody(api_response)
     case_json = case_json_by_id(case_id)
     status = case_json["casebody"]["status"]
-    if status == "ok":
-        try:
-            # Check expected keys exist
-            num_opinions = len(case_json["casebody"]["data"]["opinions"])
-        except KeyError:
-            print(f"{docket_number} num_opinions not accessible")
-            return
 
-        save_status = ""  # 'success' or will tell us which opinions are missing authors
-        for i, opinion in enumerate(case_json["casebody"]["data"]["opinions"]):
-            authors = []
-            save_result = save_opinion(case_id, docket_number,
-                                       api_response["decision_date"], opinion, i)
-            if save_result["status"] == "success":
+    if status != "ok":
+        # e.g. 'error_limit_exceeded'
+        raise RuntimeError(f"Bad API response status: {status}")
+
+    try:
+        # Check expected keys exist
+        num_opinions = len(case_json["casebody"]["data"]["opinions"])
+    except KeyError:
+        # Case doesn't have 'opinions' key
+        db_params = {"case_status": "opinions_inaccessible",
+                     "is_authors_known": 0,
+                     "selected_case_id": None,
+                     "num_opinions": None,
+                     "authors": None,
+                     "docket_number": docket_number}
+        return db_params
+
+    case_status = "success"
+    authors_known = 1  # Do we have an author for every opinion in the case?
+    authors = []
+    for i, opinion in enumerate(case_json["casebody"]["data"]["opinions"]):
+        save_result = save_opinion(case_id, docket_number, case_json["decision_date"], opinion, i)
+        if save_result["status"] == "success":
+            author_known = save_result["known_author"]
+            if author_known:
                 authors.append(save_result["author"])
-            else:
-                save_status += save_result["status"]
-        if save_status == "":
-            save_status = "success"
+            authors_known = authors_known and author_known
         else:
-            save_status = save_status[:-1]  # Remove trailing comma from last opinion with missing author
-        log_writer.writerow({"docket_number": docket_number, "status": save_status, "cases_returned": count,
-                            "case_id_selected": case_id, "num_opinions": num_opinions, "authors": "|".join(authors)})
-    elif status == "error_limit_exceeded":
-        log_writer.writerow({"docket_number": docket_number, "status": "limit_exceeded", "cases_returned": count,
-                            "case_id_selected": case_id, "num_opinions": None, "authors": None})
+            case_status = "failed"
+            print(f"Docket number {docket_number}: {save_result['status']}")
 
-    else:
-        log_writer.writerow({"docket_number": docket_number, "status": status, "cases_returned": count,
-                            "case_id_selected": case_id, "num_opinions": None, "authors": None})
+    db_params = {"case_status": case_status,
+                 "docket_number": docket_number,
+                 "is_authors_known": authors_known,
+                 "selected_case_id": case_id,
+                 "num_opinions": num_opinions,
+                 "authors": "|".join(authors),
+                 "docket_number": docket_number}
+    return db_params
 
 
-def save_opinion(case_id, docket_number, decision_date, opinion: dict, opinion_num) -> str:
-    """Saves an opinion as a txt file and returns status and authors."""
-    if opinion["author"] is None:
-        return {"status": f"op_{opinion_num}_no_author,", "author": None}
-    author = opinion["author"].lower().replace(" ", "_")
+def save_opinion(case_id, docket_number, decision_date, opinion: dict, opinion_num) -> dict:
+    """Saves an opinion as a .json file and returns dict.
+
+    Returns dict with keys:
+      - status: 'success' or op_{num}_no_author
+      - known_author: 0 or 1
+      - author: str or None
+    """
+    if (author := opinion["author"]) is None:
+        return {"status": f"op_{opinion_num}_no_author,", "known_author": 0, "author": None}
+
+    author = author.lower().replace(" ", "_")
+    known_author = 0
+    if (known_author := AUTHOR_MAP.get(author, None)) is not None:
+        author = known_author
+        known_author = 1
+
     opinion_type = opinion["type"].lower()
     text = opinion["text"]
     simplified_json = {"cap_id": case_id, "docket_number": docket_number, "decision_date": decision_date,
                        "author": author, "opinion_type": opinion_type, "text": text}
-    save_dir = Path(f"data/harvard_cap/{author}")
-    Path.mkdir(save_dir, exist_ok=True)
+    if known_author:
+        save_dir = Path(f"data/cap/known_authors/{author}")
+    else:
+        save_dir = Path(f"data/cap/unknown_authors/{author}")
+    Path.mkdir(save_dir, parents=True, exist_ok=True)
     with open(Path(save_dir, f"{docket_number}.json"), "w") as f:
         json.dump(simplified_json, f)
-    return {"status": "success", "author": author}
+    return {"status": "success", "known_author": known_author, "author": author}
