6f2c06c2a0ff432caf28defdef5b0ff55cacfee1
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-03-01 16:09:13 -0500

    Two-table setup for opinions and cases

---
+++ scripts/api_log_setup.sql
@@ -0,0 +1,9 @@
+DROP INDEX IF EXISTS idx_cases_docket_number; -- drop index before table
+DROP TABLE IF EXISTS cases;
+DROP TABLE IF EXISTS opinions;
+
+CREATE TABLE IF NOT EXISTS cases (docket_number UNIQUE, case_status, selected_case_id, decision_date);
+
+CREATE INDEX IF NOT EXISTS idx_cases_docket_number on cases (docket_number);
+
+CREATE TABLE IF NOT EXISTS opinions (docket_number, opinion_number, cap_author, author);
