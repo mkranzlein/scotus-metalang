5a3d84d45e75132416cd6a1b491c354080265b27
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-03-08 13:18:23 -0500

    Training script for simple bert model

---
+++ scripts/models/build_binary_token_model.py
@@ -0,0 +1,62 @@
+
+import json
+from itertools import chain
+
+import numpy as np
+import torch
+import transformers
+from torch.utils.data import DataLoader
+from scotus_metalang.core import datasets, curiam_categories
+from scotus_metalang.models import binary_token_model as btm
+
+bert_tokenizer = transformers.BertTokenizerFast.from_pretrained('bert-base-uncased', do_lower_case=True)
+bert_model = transformers.BertModel.from_pretrained("bert-base-uncased").cuda()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+BATCH_SIZE = 8  # Number of sentences per batch
+EPOCHS = 8
+LEARNING_RATE = 1e-4
+# Read curiam.json into docs, which are lists of sents
+with open("data/curiam/curiam.json", "r", encoding="utf-8") as f:
+    json_data = json.load(f)
+documents = []
+for json_doc in json_data:
+    doc_sentences = []
+    for raw_sent in json_doc["sentences"]:
+        tokens = [token["text"].lower() for token in raw_sent["tokens"]]
+        token_labels = datasets.get_multilabel(raw_sent, curiam_categories.REDUCED_CATEGORIES)
+        doc_sentences.append(datasets.Sentence(tokens, binary_token_labels=token_labels))
+    documents.append(doc_sentences)
+
+train_indices, val_indices = datasets.split_dataset(len(documents), validation_split=.3,
+                                                    seed=15, shuffle=True)
+
+train_sents = list(chain(*[documents[i] for i in train_indices]))
+val_sents = list(chain(*[documents[i] for i in val_indices]))
+
+train_dataloader = DataLoader(datasets.SentenceDataset(train_sents[:], bert_tokenizer),
+                              batch_size=BATCH_SIZE,
+                              shuffle=True,
+                              collate_fn=datasets.collate_sentences)
+
+val_dataloader = DataLoader(datasets.SentenceDataset(val_sents, bert_tokenizer),
+                            batch_size=BATCH_SIZE,
+                            shuffle=True,
+                            collate_fn=datasets.collate_sentences)
+
+model = btm.BinaryTokenModel(num_labels=4, bert_model=bert_model, device=device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+num_training_steps = int((len(train_dataloader) / BATCH_SIZE) * EPOCHS)
+scheduler = transformers.get_linear_schedule_with_warmup(optimizer,
+                                                         num_warmup_steps=num_training_steps // 20,
+                                                         num_training_steps=num_training_steps)
+
+for epoch in range(EPOCHS):
+    batch_losses = btm.train_loop(model, train_dataloader, optimizer, scheduler)
+    print(np.mean(batch_losses))
+    btm.eval_loop(model, val_dataloader, device, 4)
+
+torch.save(model, f"saved_models/binary_token_model_{EPOCHS}_epochs.pt")
