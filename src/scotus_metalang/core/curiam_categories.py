c26b085ed5003100d42c6289c6123814e3b7b1d1
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-03-08 13:18:37 -0500

    Pulled categories from hipool repo

---
+++ src/scotus_metalang/core/curiam_categories.py
@@ -0,0 +1,18 @@
+"""Possible classifier labels as constants."""
+
+ORDERED_CATEGORIES = [
+    "Focal Term",
+    "Definition",
+    "Metalinguistic Cue",
+    "Direct Quote",
+    "Legal Source",
+    "Language Source",
+    "Named Interpretive Rule",
+    "Example Use",
+    "Appeal to Meaning"]
+
+REDUCED_CATEGORIES = [
+    "Focal Term",
+    "Metalinguistic Cue",
+    "Direct Quote",
+    "Legal Source"]
