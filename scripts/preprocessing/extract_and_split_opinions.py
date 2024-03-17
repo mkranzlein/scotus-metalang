87a891ebd377155c8276cf64bdab111ef781c050
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-03-17 18:48:55 -0400

    Outlined opinion extraction process

---
+++ scripts/preprocessing/extract_and_split_opinions.py
@@ -0,0 +1,9 @@
+# Load map of SCDB to CAP
+
+# Load SCDB for cross-referencing
+
+# Get all the easy opinions first
+
+# For those with inexact matches, run Nathan's heuristic of "Justice [NAME],"
+
+# Evaluate match results
