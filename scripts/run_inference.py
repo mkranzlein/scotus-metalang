d3ed32adca1c8d43cd90ebe47698314d1b57988b
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-04-02 12:07:41 -0400

    Inference for new preprocessing pipeline

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,30 +1,27 @@
-
+import os
 from pathlib import Path
-from time import perf_counter
 
 import numpy as np
 import torch
-
+from dotenv import load_dotenv
+from tqdm import tqdm
 from scotus_metalang.diachronic_analysis import inference
 
-
-model_name = "binary_token_model_maybe_bert_large_8epochs"
+model_name = "binary_token_model_bert_large_8_epochs"
 model = torch.load(f"saved_models/{model_name}.pt")
 model.eval()
 
-t1 = perf_counter()
+load_dotenv()
+data_path = os.environ["SCOTUS_METALANG_DATA_PATH"]
 
-for i, opinion_path in enumerate(Path("data/cap/known_authors").glob("*/*.json")):
-    if i % 50 == 0:
-        print(i)
+data_paths = list(Path(f"{data_path}/cap/known_authors").glob("*/*.json"))
+print("Running inference on opinions and saving predictions...")
+for i, opinion_path in enumerate(tqdm(data_paths, ncols=80)):
     origin_dir = opinion_path.parent.name
     filename = opinion_path.stem + ".txt"
-    save_dir = Path("data/predictions", model_name, origin_dir)
+    save_dir = Path(f"{data_path}/predictions", model_name, origin_dir)
     if Path(save_dir, filename).exists():
         continue
     predictions = inference.predict_opinion(model, opinion_path)
     Path.mkdir(save_dir, exist_ok=True, parents=True)
     np.savetxt(Path(save_dir, filename), predictions, fmt="%.5f")
-
-t2 = perf_counter()
-print(t2 - t1)
