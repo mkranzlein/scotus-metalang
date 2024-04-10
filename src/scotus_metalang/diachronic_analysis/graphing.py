ae13081619919f212a856dbff7bd362063a69107
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-04-10 14:28:23 -0400

    Changed title to filename

--- src/scotus_metalang/diachronic_analysis/graphing.py
+++ src/scotus_metalang/diachronic_analysis/graphing.py
@@ -2,14 +2,14 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 
 
-def save_and_show(fig: plt.Figure, title: str, prefix: str = None, show: bool = False) -> None:
+def save_and_show(fig: plt.Figure, filename: str, prefix: str = None, show: bool = False) -> None:
     """Saves and optionally shows a figure."""
     if prefix:
         save_dir = Path("figures", prefix)
     else:
         save_dir = Path("figures")
     Path.mkdir(save_dir, exist_ok=True, parents=True)
-    save_path = Path(save_dir, f"{title}.pdf")
+    save_path = Path(save_dir, f"{filename}.pdf")
     fig.savefig(save_path, bbox_inches="tight")
     if show:
         plt.show()
