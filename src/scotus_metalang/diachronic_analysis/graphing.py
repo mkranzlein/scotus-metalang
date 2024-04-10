fb61dc824489d4798922064a10d9f972a26245db
Michael Kranzlein <8162250+mkranzlein@users.noreply.github.com>
2024-04-10 12:30:17 -0400

    Add prefix and fix missing dirs

--- src/scotus_metalang/diachronic_analysis/graphing.py
+++ src/scotus_metalang/diachronic_analysis/graphing.py
@@ -1,9 +1,16 @@
 from matplotlib import pyplot as plt
+from pathlib import Path
 
 
-def save_and_show(fig: plt.Figure, title: str, show: bool = False) -> None:
-    """Saves and optionally shows figure."""
-    fig.savefig(f"figures/{title}.pdf", bbox_inches="tight")
+def save_and_show(fig: plt.Figure, title: str, prefix: str = None, show: bool = False) -> None:
+    """Saves and optionally shows a figure."""
+    if prefix:
+        save_dir = Path("figures", prefix)
+    else:
+        save_dir = Path("figures")
+    Path.mkdir(save_dir, exist_ok=True, parents=True)
+    save_path = Path(save_dir, f"{title}.pdf")
+    fig.savefig(save_path, bbox_inches="tight")
     if show:
         plt.show()
     plt.close(fig)
